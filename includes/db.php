31d396a6d3075f99ba7c9b122eb2d803b97e5c4f
SANJANA SULAKSHANA WITHARANAGE <55137143+sanjana-s99@users.noreply.github.com>
2020-10-24 21:42:59 +0530

    add db connection and auth

---
+++ includes/db.php
@@ -0,0 +1,10 @@
+<?php
+    $con = mysqli_connect("localhost","root","","HARVESTGRID"); //db connection
+    if (mysqli_connect_errno())
+    {
+        echo "Failed to connect to MySQL : " . mysqli_connect_error();
+    }
+
+    date_default_timezone_set('Asia/Colombo');	
+    $error=""
+?>
